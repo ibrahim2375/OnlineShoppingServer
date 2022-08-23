5da8b0d8aef3554f681b7ed94366fae4fe54016e
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-08-23 14:39:39 +0200

    initilize server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+})
+
+
+
+
+
+//server variables
+const port = process.env.PORT || 5000;
+const host = process.env.SERVER_HOST || 'localhost';
+//server running
+app.listen(port, host, () => {
+    console.log(`server running on port ${port} : Link : http://${host}:${port}`);
+});
