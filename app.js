4d6a3c799b9f298a42e9ebf9d84fac40d8759fb3
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-08-23 14:44:36 +0200

    up

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
-
-
 app.get('/', (req, res) => {
     res.send('Welcome');
 })
-
-
-
 
 
 //server variables
